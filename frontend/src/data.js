2a8bac5b7b93a899b5cbabb600f89ed69d3b8592
AndreiDarii <andrei_darii@yahoo.com>
2020-11-20 10:45:25 +0200

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Polo Ralph Lauren Shirt',
+            category: 'Shirts',
+            image: './images/p3.jpg',
+            price: 124,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: './images/p2.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'Nike slim pants',
+            category: 'Shirts',
+            image: './images/p4.jpg',
+            price: 155,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'Nike classic Shirt',
+            category: 'Shirts',
+            image: './images/p1.jpg',
+            price: 120,
+            brand: 'Polo',
+            rating: 5.0,
+            numReviews: 22,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'Polo Ralph Lauren pants',
+            category: 'Pants',
+            image: './images/p5.jpg',
+            price: 60,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 2,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'Adidas slim pants',
+            category: 'Pants',
+            image: './images/p6.jpg',
+            price: 80,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 11,
+            description: 'high quality product'
+        }
+
+    ]
+};
+
+export default data;
