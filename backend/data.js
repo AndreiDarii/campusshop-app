c51347449745658931e01afa9c6e846bec141bba
AndreiDarii <andrei_darii@yahoo.com>
2020-11-21 14:19:46 +0200

    created node.js server

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products:[
+        {
+            _id:'1',
+            name: 'Polo Ralph Lauren Shirt',
+            category: 'Shirts',
+            image: '/images/p3.jpg',
+            price: 124,
+            countInStock: 30,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 12,
+            description: 'high quality product'
+        },
+        {
+            _id:'2',
+            name: 'Adidas Fit Shirt',
+            category: 'Shirts',
+            image: '/images/p2.jpg',
+            price: 100,
+            countInStock: 111,
+            brand: 'Adidas',
+            rating: 4.0,
+            numReviews: 10,
+            description: 'high quality product'
+        },
+        {
+            _id:'3',
+            name: 'Nike slim pants',
+            category: 'Shirts',
+            image: '/images/p4.jpg',
+            price: 155,
+            countInStock: 51,
+            brand: 'Polo',
+            rating: 3.5,
+            numReviews: 13,
+            description: 'high quality product'
+        },
+        {
+            _id:'4',
+            name: 'Nike classic Shirt',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 15,
+            brand: 'Polo',
+            rating: 2.0,
+            numReviews: 22,
+            description: 'high quality product'
+        },
+        {
+            _id:'5',
+            name: 'Polo Ralph Lauren pants',
+            category: 'Pants',
+            image: '/images/p5.jpg',
+            price: 60,
+            countInStock: 1,
+            brand: 'Polo',
+            rating: 4.5,
+            numReviews: 2,
+            description: 'high quality product'
+        },
+        {
+            _id:'6',
+            name: 'Adidas slim pants',
+            category: 'Pants',
+            image: '/images/p6.jpg',
+            price: 80,
+            countInStock: 0,
+            brand: 'Adidas',
+            rating: 3.5,
+            numReviews: 11,
+            description: 'high quality merge product'
+        }
+
+    ]
+};
+
+export default data;
