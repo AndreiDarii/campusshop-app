d686b75365f0326c83478e08c2f934ac285cfc10
AndreiDarii <andrei_darii@yahoo.com>
2020-11-23 13:43:14 +0200

    Add Redux to homeScreen

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import { productListReducer } from './reducers/productReducers';
+
+const initialState = {};
+const reducer = combineReducers({
+    productList: productListReducer,
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    reducer, 
+    initialState, 
+    composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
