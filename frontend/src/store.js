d2e88336e0b6f0933e5c601944458376e41a890a
AndreiDarii <andrei_darii@yahoo.com>
2020-12-02 13:53:52 +0200

    place order actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, createStore, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import { cartReducer } from './reducers/cartReducers';
+import { orderCreateReducer } from './reducers/orderReducers';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import { userRegisterReducer, userSignInReducer } from './reducers/userReducers';
 
@@ -26,6 +27,7 @@ const reducer = combineReducers({
     cart: cartReducer,
     userSignin: userSignInReducer,
     userRegister: userRegisterReducer,
+    orderCreate: orderCreateReducer
 })
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
